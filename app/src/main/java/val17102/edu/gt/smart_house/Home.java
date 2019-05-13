adb3ee1ca821c361cd290267ba190bc21d4d8dd1
val17102 <val17102@uvg.edu.gt>
2019-05-12 20:11:04 -0600

    Menu principal y Video

---
+++ app/src/main/java/val17102/edu/gt/smart_house/Home.java
@@ -0,0 +1,24 @@
+package val17102.edu.gt.smart_house;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        CardView videoButton = findViewById(R.id.video_button);
+        videoButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(Home.this, Video.class));
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/val17102/edu/gt/smart_house/Video.java
@@ -0,0 +1,24 @@
+package val17102.edu.gt.smart_house;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class Video extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+        VideoView videoView= findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/"+ R.raw.video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+    }
+}
